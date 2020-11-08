7d646ed0070482515ea7309b77dedea0e5d9e4b8
rasid bram <rashidaynur.ibram@yandex.com>
2020-11-08 15:17:33 +0000

    added mongo atlas URI

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,4 +1,3 @@
 module.exports = {
-    // mongoURI:"mongodb+srv://rasidbram1:12345abcde@cluster0-hbtpq.mongodb.net/MarketDB"
-    mongoURI:"mongodb://127.0.0.1:27017/marketDB"
+    mongoURI:"mongodb+srv://rashidShopApp:12345abcde@cluster0-hbtpq.mongodb.net/MarketDB?retryWrites=true&w=majority"
 }
